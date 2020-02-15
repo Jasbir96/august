d05317667b00cf4b51c17e769c178e67169b2ace
Jasbir <mr.jasbirsingh96@gmail.com>
2020-02-15 18:54:01 +0530

    added some images

--- controllers/userController.js
+++ controllers/userController.js
@@ -10,6 +10,7 @@ module.exports.getUser = async function (req, res) {
 module.exports.updateUser = async function (req, res) {
   const id = req.params.id;
   const img=await sharp(req.file.buffer).resize(600, 600).toFile("public/"+req.file.fieldname + '-' + Date.now() + ".jpeg")
+  console.log(img);
   req.body.photo = req.file.fieldname + '-' + Date.now() + ".jpeg";
   
   const user = await userModel.findOneAndUpdate({ _id: id }, req.body, {
