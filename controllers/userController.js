bec8641798c0de9d5083eecb1b385d3f88b33de3
Jasbir <mr.jasbirsingh96@gmail.com>
2020-02-15 18:40:02 +0530

    added image

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,4 +1,5 @@
 const sharp = require("sharp");
+const fs=require("fs")
 const userModel = require("../models/userModel");
 module.exports.getUser = async function (req, res) {
   const { id } = req.body;
@@ -8,10 +9,12 @@ module.exports.getUser = async function (req, res) {
 // update
 module.exports.updateUser = async function (req, res) {
   const id = req.params.id;
-  await sharp(req.file.buffer).resize(600, 600).jpeg({
+  const img=await sharp(req.file.buffer).resize(600, 600).jpeg({
     quality: 60
   }).toFile("public/"+req.file.fieldname + '-' + Date.now() + ".jpeg")
   req.body.photo = req.file.fieldname + '-' + Date.now() + ".jpeg";
+  const Imgfile=fs.createWriteStream(req.body.photo);
+  img.pipe(Imgfile);
   const user = await userModel.findOneAndUpdate({ _id: id }, req.body, {
     new: true
   });
